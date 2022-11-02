72245677b8bce94c6fb4cb4a12b5e4b721edbe2f
malik ahmed <atondaahmed@gmail.com>
2022-11-02 14:04:35 +0100

    addeD django-on-heroku

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -133,3 +133,7 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
+
+# Configure Django App for Heroku.
+import django_on_heroku
+django_on_heroku.settings(locals())
